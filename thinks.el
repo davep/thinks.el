f5a339b21cd5044f7b66beafab7c2d822c36f9e5
Dave Pearson <davep@davep.org>
2017-04-18 16:54:59 +0100

    v1.11

--- thinks.el
+++ thinks.el
@@ -2,7 +2,7 @@
 ;; Copyright 2000-2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.10
+;; Version: 1.11
 ;; Keywords: convenience, quoting
 ;; URL: https://github.com/davep/thinks.el
 ;; Package-Requires: ((cl-lib "0.5"))
