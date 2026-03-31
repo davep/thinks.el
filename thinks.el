6270e2c1740e1eba2b5c6beb7c9e26e2c795bf88
Dave Pearson <davep@davep.org>
2026-03-31 19:11:05 +0100

    :pushpin: Pin the minimum version of Emacs

--- thinks.el
+++ thinks.el
@@ -5,7 +5,7 @@
 ;; Version: 1.12
 ;; Keywords: convenience, quoting
 ;; URL: https://github.com/davep/thinks.el
-;; Package-Requires: ((cl-lib "0.5"))
+;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
 
 ;; This program is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the
